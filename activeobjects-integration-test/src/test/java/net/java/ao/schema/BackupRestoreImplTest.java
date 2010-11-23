29bae55ab9977ffd03ac56b58db2aa150d2a864e
Samuel Le Berrigaud <sam@leberrigaud.org>
2010-11-23 13:47:17 +0000

    Extracting model for integration testing into its own module. This should enable more flexible integration testing for libraries using AO.

--- activeobjects-integration-test/src/test/java/net/java/ao/schema/BackupRestoreImplTest.java
+++ activeobjects-integration-test/src/test/java/net/java/ao/schema/BackupRestoreImplTest.java
@@ -4,9 +4,6 @@ import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import net.java.ao.DatabaseProvider;
-import net.java.ao.DefaultSchemaConfiguration;
-import net.java.ao.EntityManager;
 import net.java.ao.RawEntity;
 import net.java.ao.it.model.backup.Animal;
 import net.java.ao.it.model.backup.AnimalClass;
@@ -18,13 +15,9 @@ import net.java.ao.test.jdbc.Hsql;
 import net.java.ao.test.jdbc.Jdbc;
 import net.java.ao.test.jdbc.NonTransactional;
 import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -32,7 +25,6 @@ import java.util.List;
 import static com.google.common.collect.Iterables.*;
 import static com.google.common.collect.Lists.*;
 import static net.java.ao.schema.BackupRestoreDatabaseUpdater.*;
-import static net.java.ao.sql.SqlUtils.*;
 import static org.junit.Assert.*;
 
 /**
@@ -41,27 +33,8 @@ import static org.junit.Assert.*;
 @Data(BackupRestoreDatabaseUpdater.class)
 @Jdbc(Hsql.class)
 @RunWith(ActiveObjectsJUnitRunner.class)
-public class BackupRestoreImplTest
+public class BackupRestoreImplTest extends AbstractBackupRestoreTest
 {
-    private BackupRestore backupRestore;
-
-    private EntityManager entityManager;
-    private DefaultSchemaConfiguration schemaConfiguration;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        backupRestore = new BackupRestoreImpl();
-        schemaConfiguration = new DefaultSchemaConfiguration();
-    }
-
-    @After
-    public void tearDown() throws Exception
-    {
-        schemaConfiguration = null;
-        backupRestore = null;
-    }
-
     @Test
     @NonTransactional
     public void testBackupAndRestore() throws Exception
@@ -138,16 +111,6 @@ public class BackupRestoreImplTest
         }
     }
 
-    private void restore(List<DDLAction> backup)
-    {
-        backupRestore.restore(backup, getProvider());
-    }
-
-    private List<DDLAction> backup()
-    {
-        return backupRestore.backup(getProvider(), schemaConfiguration);
-    }
-
     private static void assertAnimalClassesExists(Collection<AnimalClass> animalClasses, final AnimalClassData animalClassData)
     {
         assertExists(animalClasses, new Predicate<AnimalClass>()
@@ -176,40 +139,6 @@ public class BackupRestoreImplTest
     private static <E> void assertExists(Collection<E> entities, Predicate<E> entityPredicate)
     {
         assertEquals("Should have found one and only one", 1, size(filter(entities, entityPredicate)));
-    }
-
-    private void emptyDatabase() throws Exception
-    {
-        entityManager.migrate(); // removes all data from the database
-        entityManager.flushAll();
-        assertDatabaseIsEmpty();
-    }
-
-    private void assertDatabaseIsEmpty() throws Exception
-    {
-        assertDatabaseIsEmpty(true);
-    }
-
-    private void assertDatabaseIsNotEmpty() throws Exception
-    {
-        assertDatabaseIsEmpty(false);
-    }
-
-    private void assertDatabaseIsEmpty(boolean isEmpty) throws Exception
-    {
-        Connection connection = null;
-        ResultSet tables = null;
-        try
-        {
-            connection = getProvider().getConnection();
-            tables = getProvider().getTables(connection);
-            assertEquals(!isEmpty, tables.next());
-        }
-        finally
-        {
-            closeQuietly(tables);
-            closeQuietly(connection);
-        }
     }
 
     private void assertIsDropTables(Iterator<DDLAction> ddlActions, Class<? extends RawEntity<?>>... entityClass)
@@ -252,25 +181,5 @@ public class BackupRestoreImplTest
     private boolean tableNamesEquals(Class<? extends RawEntity<?>> entityClass, DDLTable ddlTable)
     {
         return getTableNameIgnoreCase(getTableName(entityClass)).equals(getTableNameIgnoreCase(ddlTable.getName()));
-    }
-
-    private String getTableNameIgnoreCase(String originalTableName)
-    {
-        return getProvider().isCaseSensetive() ? originalTableName : originalTableName.toLowerCase();
-    }
-
-    private String getTableName(Class<? extends RawEntity<?>> entityClass)
-    {
-        return getTableNameConverter().getName(entityClass);
-    }
-
-    private TableNameConverter getTableNameConverter()
-    {
-        return entityManager.getTableNameConverter();
-    }
-
-    private DatabaseProvider getProvider()
-    {
-        return entityManager.getProvider();
     }
 }
