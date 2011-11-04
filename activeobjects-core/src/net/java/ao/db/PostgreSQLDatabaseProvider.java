00cd46048e6fa355dbad80f8915000d9c12d989c
Samuel Le Berrigaud <sam@leberrigaud.org>
2011-11-04 16:07:05 +0100

    Oops, fixing for Postgres.

--- activeobjects-core/src/net/java/ao/db/PostgreSQLDatabaseProvider.java
+++ activeobjects-core/src/net/java/ao/db/PostgreSQLDatabaseProvider.java
@@ -144,7 +144,7 @@ public class PostgreSQLDatabaseProvider extends DatabaseProvider {
             case Types.DATE:
             case Types.TIME:
             case Types.VARCHAR:
-				Matcher matcher = Pattern.compile("'(.+)'.*").matcher(value);
+				Matcher matcher = Pattern.compile("'(.*)'.*").matcher(value);
 				if (matcher.find()) {
 					value = matcher.group(1);
 				}
