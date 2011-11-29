27a30c243d8395fc64d6ebdb7f21007272113140
Samuel Le Berrigaud <sam@leberrigaud.org>
2011-11-29 10:42:55 +0100

    Oops, committed local database jdbc configuration, reverting.

--- activeobjects-test/src/main/java/net/java/ao/test/jdbc/Oracle.java
+++ activeobjects-test/src/main/java/net/java/ao/test/jdbc/Oracle.java
@@ -7,12 +7,12 @@ public class Oracle extends AbstractJdbcConfiguration
 {
     public String getUrl()
     {
-        return "jdbc:oracle:thin:@192.168.0.168:1521:orcl";
+        return "jdbc:oracle:thin:@localhost:1521:orcl";
     }
 
-//    @Override
-//    public String getSchema()
-//    {
-//        return "ao_schema";
-//    }
+    @Override
+    public String getSchema()
+    {
+        return "ao_schema";
+    }
 }
