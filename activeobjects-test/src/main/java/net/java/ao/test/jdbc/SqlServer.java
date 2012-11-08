e1ebffbdd5a3b579eb7ef3893f28852b5d6b578b
Justin Koke <jkoke@atlassian.com>
2012-11-08 12:52:50 +1100

    Commit for AO-379
    Adding support for passing environment variables to the testing framework for AO for
    all database properties, url, username, password etc.

--- activeobjects-test/src/main/java/net/java/ao/test/jdbc/SqlServer.java
+++ activeobjects-test/src/main/java/net/java/ao/test/jdbc/SqlServer.java
@@ -2,14 +2,29 @@ package net.java.ao.test.jdbc;
 
 public class SqlServer extends AbstractJdbcConfiguration
 {
-    @Override
-    public String getSchema()
+
+    public static final String DEFAULT_SCHEMA = "ao_schema";
+    public static final String DEFAULT_URL = "jdbc:jtds:sqlserver://localhost:1433;DatabaseName=ao_test";
+
+    public SqlServer()
     {
-        return "ao_schema";
+        super(DEFAULT_URL, DEFAULT_USER, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
     }
 
-    public String getUrl()
+    public SqlServer(String url, String username, String password, String schema)
     {
-        return "jdbc:jtds:sqlserver://localhost:1433;DatabaseName=ao_test";
+        super(url, username, password, schema);
+    }
+
+    @Override
+    protected String getDefaultSchema()
+    {
+        return DEFAULT_SCHEMA;
+    }
+
+    @Override
+    protected String getDefaultUrl()
+    {
+        return DEFAULT_URL;
     }
 }
