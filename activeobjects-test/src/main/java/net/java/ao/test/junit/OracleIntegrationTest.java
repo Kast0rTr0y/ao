500d79ab216635069160568aff7d75e88343f36d
Justin Koke <jkoke@atlassian.com>
2012-12-10 12:09:59 +1100

    AO-379 - Separating some of the integration tests.
    Tests are meant to only be run against their target database, but it seems that the POM's don't reflect
    this. I have added marker interfaces and started using JUnit4.7+ Condional annotations to help
    segregate some of these tests so they only run against the given database type.

---
+++ activeobjects-test/src/main/java/net/java/ao/test/junit/OracleIntegrationTest.java
@@ -0,0 +1,9 @@
+package net.java.ao.test.junit;
+
+/**
+ * This is a marker interface to be used by JUnit categories
+ * to be used by surefire when selecting which tests should be run
+ */
+public interface OracleIntegrationTest
+{
+}
