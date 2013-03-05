7101036c672081aecbfaa23ba4c0716a2f1273f2
Richard Atkins <richatkins@atlassian.com>
2013-03-05 16:08:24 +1100

    AO-220 Add (disabled) support for using HSQLDB on disk instead of a purely in memory DB.

--- activeobjects-test/src/main/java/net/java/ao/test/jdbc/Hsql.java
+++ activeobjects-test/src/main/java/net/java/ao/test/jdbc/Hsql.java
@@ -1,15 +1,20 @@
 package net.java.ao.test.jdbc;
 
+import net.java.ao.test.ConfigurationProperties;
+
 public class Hsql extends AbstractJdbcConfiguration
 {
-    private static final String DEFAULT_URL = "jdbc:hsqldb:mem:ao_test";
+    private static final String TEST_HOME_CONSTANT = "${testHome}"; // to be replaced by replaceTestHomeConstant
+    private static final String FILE_URL = "jdbc:hsqldb:${testHome}/database/ao_test";
+    private static final String IN_MEMORY_URL = "jdbc:hsqldb:mem:ao_test";
     private static final String DEFAULT_USERNAME = "sa";
     private static final String DEFAULT_PASSWORD = "";
     private static final String DEFAULT_SCHEMA = "PUBLIC";
 
     public Hsql()
     {
-        super(DEFAULT_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
+//        super(getFileUrl(), DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
+        super(IN_MEMORY_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
     }
 
     public Hsql(String url, String username, String password, String schema)
@@ -38,6 +43,37 @@ public class Hsql extends AbstractJdbcConfiguration
     @Override
     protected String getDefaultUrl()
     {
-        return DEFAULT_URL;
+//        return getFileUrl();
+        return IN_MEMORY_URL;
+    }
+
+    private static String getFileUrl()
+    {
+        return replaceTestHomeConstant(FILE_URL, getTestHome());
+    }
+
+    private static String replaceTestHomeConstant(String in, String testHome)
+    {
+        if (in == null)
+        {
+            return null;
+        }
+
+        StringBuilder prop = new StringBuilder(in);
+
+        int length = TEST_HOME_CONSTANT.length();
+        int index = prop.indexOf(TEST_HOME_CONSTANT);
+
+        while (index != -1)
+        {
+            prop.replace(index, index + length, testHome);
+            index = prop.indexOf(TEST_HOME_CONSTANT);
+        }
+        return prop.toString();
+    }
+
+    private static String getTestHome()
+    {
+        return ConfigurationProperties.get("test.home", "target/testHome");
     }
 }
