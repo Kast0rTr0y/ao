675c602aa77fdfd833b211c6c1150c9f47c975ef
Richard Atkins <richatkins@atlassian.com>
2013-03-07 14:47:45 +1100

    AO-220 Pull file storage handling for HSQLDB into its own JdbcConfiguration, so we allow overriding settings correctly

--- activeobjects-test/src/main/java/net/java/ao/test/jdbc/Hsql.java
+++ activeobjects-test/src/main/java/net/java/ao/test/jdbc/Hsql.java
@@ -1,11 +1,7 @@
 package net.java.ao.test.jdbc;
 
-import net.java.ao.test.ConfigurationProperties;
-
 public class Hsql extends AbstractJdbcConfiguration
 {
-    private static final String TEST_HOME_CONSTANT = "${testHome}"; // to be replaced by replaceTestHomeConstant
-    private static final String FILE_URL = "jdbc:hsqldb:${testHome}/database/ao_test";
     private static final String IN_MEMORY_URL = "jdbc:hsqldb:mem:ao_test";
     private static final String DEFAULT_USERNAME = "sa";
     private static final String DEFAULT_PASSWORD = "";
@@ -13,8 +9,12 @@ public class Hsql extends AbstractJdbcConfiguration
 
     public Hsql()
     {
-//        super(getFileUrl(), DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
-        super(IN_MEMORY_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
+        this(IN_MEMORY_URL);
+    }
+
+    protected Hsql(String url)
+    {
+        this(url, DEFAULT_USERNAME, DEFAULT_PASSWORD, DEFAULT_SCHEMA);
     }
 
     public Hsql(String url, String username, String password, String schema)
@@ -43,37 +43,6 @@ public class Hsql extends AbstractJdbcConfiguration
     @Override
     protected String getDefaultUrl()
     {
-//        return getFileUrl();
         return IN_MEMORY_URL;
-    }
-
-    private static String getFileUrl()
-    {
-        return replaceTestHomeConstant(FILE_URL, getTestHome());
-    }
-
-    private static String replaceTestHomeConstant(String in, String testHome)
-    {
-        if (in == null)
-        {
-            return null;
-        }
-
-        StringBuilder prop = new StringBuilder(in);
-
-        int length = TEST_HOME_CONSTANT.length();
-        int index = prop.indexOf(TEST_HOME_CONSTANT);
-
-        while (index != -1)
-        {
-            prop.replace(index, index + length, testHome);
-            index = prop.indexOf(TEST_HOME_CONSTANT);
-        }
-        return prop.toString();
-    }
-
-    private static String getTestHome()
-    {
-        return ConfigurationProperties.get("test.home", "target/testHome");
     }
 }
