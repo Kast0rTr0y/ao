7125afdc3dc8d4e6f8a3096272cede3e23122a0a
James Gorman <jgorman@atlassian.com>
2013-11-27 12:25:53 +1100

    AO-458: After discussion with @jhinch I’m setting the test defaults to those used by the default Atlassian install (the unique index for table names is fixed). This will make tests more consistent with deployment, though may break some builds.

--- activeobjects-test/src/main/java/net/java/ao/test/converters/NameConverters.java
+++ activeobjects-test/src/main/java/net/java/ao/test/converters/NameConverters.java
@@ -1,27 +1,25 @@
 package net.java.ao.test.converters;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
 import net.java.ao.EntityManager;
-import net.java.ao.schema.DefaultIndexNameConverter;
-import net.java.ao.schema.DefaultSequenceNameConverter;
-import net.java.ao.schema.DefaultTriggerNameConverter;
-import net.java.ao.schema.DefaultUniqueNameConverter;
+import net.java.ao.atlassian.AtlassianIndexNameConverter;
+import net.java.ao.atlassian.AtlassianSequenceNameConverter;
+import net.java.ao.atlassian.AtlassianTriggerNameConverter;
+import net.java.ao.atlassian.AtlassianUniqueNameConverter;
 import net.java.ao.schema.FieldNameConverter;
 import net.java.ao.schema.IndexNameConverter;
 import net.java.ao.schema.SequenceNameConverter;
 import net.java.ao.schema.TableNameConverter;
 import net.java.ao.schema.TriggerNameConverter;
 import net.java.ao.schema.UniqueNameConverter;
-import net.java.ao.test.jdbc.DatabaseUpdater;
-import net.java.ao.test.jdbc.NonTransactional;
 import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
 
-import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.RetentionPolicy.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Use with {@link ActiveObjectsJUnitRunner} to specify implementation classes for
@@ -49,20 +47,20 @@ public @interface NameConverters
     /**
      * Specifies an implementation of {@link SequenceNameConverter}.
      */
-    Class<? extends SequenceNameConverter> sequence() default DefaultSequenceNameConverter.class;
+    Class<? extends SequenceNameConverter> sequence() default AtlassianSequenceNameConverter.class;
 
     /**
      * Specifies an implementation of {@link TriggerNameConverter}.
      */
-    Class<? extends TriggerNameConverter> trigger() default DefaultTriggerNameConverter.class;
+    Class<? extends TriggerNameConverter> trigger() default AtlassianTriggerNameConverter.class;
 
     /**
      * Specifies an implementation of {@link IndexNameConverter}.
      */
-    Class<? extends IndexNameConverter> index() default DefaultIndexNameConverter.class;
+    Class<? extends IndexNameConverter> index() default AtlassianIndexNameConverter.class;
 
     /**
      * Specifies an implementation of {@link UniqueNameConverter}.
      */
-    Class<? extends UniqueNameConverter> unique() default DefaultUniqueNameConverter.class;
+    Class<? extends UniqueNameConverter> unique() default AtlassianUniqueNameConverter.class;
 }
