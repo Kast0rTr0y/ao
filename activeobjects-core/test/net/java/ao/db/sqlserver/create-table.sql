96d8550d99451db319661538eba3b3eeb6e1e94b
Alexander Courtis <acourtis@atlassian.com>
2014-08-27 10:16:48 +1000

    AO-386 SQL server uses NVARCHAR instead of VARCHAR to allow non-latin characters

--- activeobjects-core/test/net/java/ao/db/sqlserver/create-table.sql
+++ activeobjects-core/test/net/java/ao/db/sqlserver/create-table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE dbo.person (
     id INTEGER IDENTITY(1,1) NOT NULL,
-    firstName VARCHAR(255) NOT NULL,
+    firstName NVARCHAR(255) NOT NULL,
     lastName NTEXT,
     age INTEGER,
-    url VARCHAR(767) CONSTRAINT U_person_url UNIQUE NOT NULL,
+    url NVARCHAR(767) CONSTRAINT U_person_url UNIQUE NOT NULL,
     height FLOAT CONSTRAINT df_person_height DEFAULT 62.3,
     companyID BIGINT,
     cool BIT CONSTRAINT df_person_cool DEFAULT 1,
     modified DATETIME,
     weight FLOAT,
-    typeOfPerson VARCHAR(30),
+    typeOfPerson NVARCHAR(30),
     CONSTRAINT fk_person_companyid FOREIGN KEY (companyID) REFERENCES dbo.company(id),
 CONSTRAINT pk_person_id PRIMARY KEY(id)
 )
