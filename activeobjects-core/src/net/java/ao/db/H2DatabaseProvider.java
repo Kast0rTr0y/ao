69b7f761be6eada6b6a098b970c18def294b6446
Alexander Courtis <acourtis@atlassian.com>
2014-09-04 20:58:41 +1000

    populate H2DatabaseProviderTest

--- activeobjects-core/src/net/java/ao/db/H2DatabaseProvider.java
+++ activeobjects-core/src/net/java/ao/db/H2DatabaseProvider.java
@@ -1,9 +1,14 @@
 package net.java.ao.db;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import net.java.ao.DatabaseProvider;
 import net.java.ao.DisposableDataSource;
 import net.java.ao.Query;
+import net.java.ao.schema.NameConverters;
+import net.java.ao.schema.ddl.DDLField;
+import net.java.ao.schema.ddl.DDLTable;
+import net.java.ao.schema.ddl.SQLAction;
 import net.java.ao.types.TypeManager;
 
 import java.util.Set;
@@ -34,6 +39,35 @@ public class H2DatabaseProvider extends DatabaseProvider
         sql.append(super.renderQueryLimit(query));
 
         return sql.toString();
+    }
+
+    @Override
+    protected Iterable<SQLAction> renderAlterTableChangeColumn(final NameConverters nameConverters, final DDLTable table, final DDLField oldField, final DDLField field)
+    {
+        final ImmutableList.Builder<SQLAction> sql = ImmutableList.builder();
+
+        if (!oldField.getName().equals(field.getName()))
+        {
+            sql.add(SQLAction.of(new StringBuilder().append("ALTER TABLE ")
+                    .append(withSchema(table.getName()))
+                    .append(" ALTER COLUMN ")
+                    .append(oldField.getName())
+                    .append(" RENAME TO ")
+                    .append(field.getName())));
+        }
+
+        sql.addAll(super.renderAlterTableChangeColumn(nameConverters, table, oldField, field));
+
+        return sql.build();
+    }
+
+    @Override
+    protected SQLAction renderAlterTableChangeColumnStatement(final NameConverters nameConverters, final DDLTable table, final DDLField oldField, final DDLField field, final RenderFieldOptions options)
+    {
+        StringBuilder current = new StringBuilder();
+        current.append("ALTER TABLE ").append(withSchema(table.getName())).append(" ALTER COLUMN ");
+        current.append(renderField(nameConverters, table, field, options));
+        return SQLAction.of(current);
     }
 
     @Override
