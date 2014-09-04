b833c2119ccd06337ccb76f68b1c3b408a459bf7
Alexander Courtis <acourtis@atlassian.com>
2014-09-04 20:13:44 +1000

    populate H2DatabaseProviderTest

---
+++ activeobjects-core/test/net/java/ao/db/h2/create-table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE PUBLIC.person (
+    id INT AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName CLOB,
+    age INT,
+    url VARCHAR(450) UNIQUE NOT NULL,
+    height DOUBLE DEFAULT 62.3,
+    companyID BIGINT,
+    cool BOOLEAN DEFAULT 1,
+    modified TIMESTAMP,
+    weight DOUBLE,
+    typeOfPerson VARCHAR(30),
+    CONSTRAINT fk_person_companyid FOREIGN KEY (companyID) REFERENCES PUBLIC.company(id),
+    PRIMARY KEY(id)
+)
