180666208f5081ef23ab1c98e79fa2b89e52470a
Sebastian Pawlak <spawlak@atlassian.com>
2016-05-24 10:27:53 +0200

    fixup

--- activeobjects-core/src/net/java/ao/schema/ddl/SchemaReader.java
+++ activeobjects-core/src/net/java/ao/schema/ddl/SchemaReader.java
@@ -222,6 +222,12 @@ public final class SchemaReader {
             DDLAction action = new DDLAction(DDLActionType.CREATE);
             action.setTable(table);
             actions.add(action);
+
+            for (DDLIndex index : table.getIndexes()) {
+                DDLAction indexAction = new DDLAction(DDLActionType.CREATE_INDEX);
+                indexAction.setIndex(index);
+                actions.add(indexAction);
+            }
         }
 
 
