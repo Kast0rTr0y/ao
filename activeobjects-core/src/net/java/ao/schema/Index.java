1e08435bc15bba72896abf4ca364901aa86b3d37
Sebastian Pawlak <spawlak@atlassian.com>
2016-06-01 17:10:28 +0200

    fixup! RUN-104: Add API for declaring composite indexes

--- activeobjects-core/src/net/java/ao/schema/Index.java
+++ activeobjects-core/src/net/java/ao/schema/Index.java
@@ -10,15 +10,21 @@ import java.lang.annotation.Target;
  *
  * <p>
  *     The index can be either simple or composite.
- *     The {@link #methods()} contains an array of accessors or mutators to the columns that should be included in the index.
- *     The {@link #name()} should specify table unique index name.
- *     Please note that the actual name of the index may differ.This field is case <strong>insensitive</strong>.
+ *     Please note that the actual name of the index may differ. This field is case <strong>insensitive</strong>.
  * </p>
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface Index {
+
+    /**
+     * @return the table unique index name.
+     */
     String name();
+
+    /**
+     * @return an array of accessors or mutators to the columns that should be included in the index.
+     */
     String[] methodNames();
 }
